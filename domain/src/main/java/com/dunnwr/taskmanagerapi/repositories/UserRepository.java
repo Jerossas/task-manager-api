0823d9e22110901a5939c30aae3d98db754b661d
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-03-15 00:22:26 -0500

    feat(user-domain): add user repository with method to implement user registration

---
+++ domain/src/main/java/com/dunnwr/taskmanagerapi/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.dunnwr.taskmanagerapi.repositories;
+
+import com.dunnwr.taskmanagerapi.models.user.User;
+
+public interface UserRepository {
+
+    User save(User user);
+    boolean existsByEmail(String email);
+}
