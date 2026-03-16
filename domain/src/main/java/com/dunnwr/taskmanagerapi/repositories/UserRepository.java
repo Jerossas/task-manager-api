6edb2859a3a0b540ba9c4e28ab3f50fdedce64fe
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-03-15 19:12:40 -0500

    feat(user-domain): add missing methods used in implementation

--- domain/src/main/java/com/dunnwr/taskmanagerapi/repositories/UserRepository.java
+++ domain/src/main/java/com/dunnwr/taskmanagerapi/repositories/UserRepository.java
@@ -3,8 +3,11 @@ package com.dunnwr.taskmanagerapi.repositories;
 import com.dunnwr.taskmanagerapi.models.user.Email;
 import com.dunnwr.taskmanagerapi.models.user.User;
 
+import java.util.Optional;
+
 public interface UserRepository {
 
     User save(User user);
     boolean existsByEmail(Email email);
+    Optional<User> findByEmail(Email email);
 }
