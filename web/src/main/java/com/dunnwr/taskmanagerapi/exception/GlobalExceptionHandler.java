424050165c5179bdd974db1fb88561fef5111335
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-03-15 20:59:56 -0500

    fea(web): add exception handler for email already used exception

--- web/src/main/java/com/dunnwr/taskmanagerapi/exception/GlobalExceptionHandler.java
+++ web/src/main/java/com/dunnwr/taskmanagerapi/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.dunnwr.taskmanagerapi.exception;
 
+import com.dunnwr.taskmanagerapi.exceptions.EmailAlreadyRegisteredException;
 import com.dunnwr.taskmanagerapi.exceptions.InvalidFieldException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,6 +15,18 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleDomainException(InvalidFieldException e){
         ErrorResponse error = new ErrorResponse(
                 e.getField(),
+                e.getMessage(),
+                e.getStatusCode(),
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(e.getStatusCode()).body(error);
+    }
+
+    @ExceptionHandler(EmailAlreadyRegisteredException.class)
+    public ResponseEntity<ErrorResponse> handleBusinessLogicException(EmailAlreadyRegisteredException e){
+        ErrorResponse error = new ErrorResponse(
+                null,
                 e.getMessage(),
                 e.getStatusCode(),
                 LocalDateTime.now()
