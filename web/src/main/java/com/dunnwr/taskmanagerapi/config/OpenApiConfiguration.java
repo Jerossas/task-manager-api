853d1120f64c37b6dd4f5d97d250871d975498fa
Joan Ferley Mosquera Lozano <mosqueralozanojhoanferley@gmail.com>
2026-05-15 17:16:09 -0500

    feat(web): add configuration for Open API.
    - Added title, version and description
    - Added support for bearer token for interaction with the UI

---
+++ web/src/main/java/com/dunnwr/taskmanagerapi/config/OpenApiConfiguration.java
@@ -0,0 +1,31 @@
+package com.dunnwr.taskmanagerapi.config;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Task Manager API")
+                        .version("1.0.0")
+                        .description("API REST para manejo de tareas personales"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
