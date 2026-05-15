f84f9ac7e10b383f471f961d74d21e1c1387f7cb
Joan Ferley Mosquera Lozano <mosqueralozanojhoanferley@gmail.com>
2026-05-14 22:21:58 -0500

    fix(test): add testing class for EditUserBasicInformationUseCaseImpl

---
+++ application/src/test/java/com/dunnwr/taskmanagerapi/usecases/user/EditUserBasicInformationUseCaseTest.java
@@ -0,0 +1,74 @@
+package com.dunnwr.taskmanagerapi.usecases.user;
+
+import com.dunnwr.taskmanagerapi.commands.user.EditUserBasicInformationCommand;
+import com.dunnwr.taskmanagerapi.exceptions.UserNotFoundException;
+import com.dunnwr.taskmanagerapi.models.user.*;
+import com.dunnwr.taskmanagerapi.repositories.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class EditUserBasicInformationUseCaseTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private EditUserBasicInformationUseCaseImpl editUserBasicInformationUseCase;
+
+    private User createValidUser() {
+        return User.restore(
+                1L,
+                "Joan",
+                "Ferley",
+                "Mosquera Lozano",
+                Password.fromEncoded("encoded-hash"),
+                Email.fromStored("example@domain.com"),
+                Gender.MALE,
+                new HashSet<>(Set.of(Role.USER))
+        );
+    }
+
+    @Test
+    void whenUserDoesNotExist_shouldReturnUserNotFound() {
+
+        when(userRepository.findByEmail(any())).thenReturn(Optional.empty());
+
+        assertThrows(UserNotFoundException.class, () -> editUserBasicInformationUseCase.execute(new EditUserBasicInformationCommand(
+                "Joan",
+                "Ferley",
+                "Mosquera Lozano",
+                "male",
+                "example@domain.com"
+        )));
+    }
+
+    @Test
+    void whenExistingUser_shouldUpdateInformation() {
+
+        when(userRepository.findByEmail(any())).thenReturn(Optional.of(createValidUser()));
+        when(userRepository.save(any())).thenAnswer(invocation -> invocation.getArgument(0));
+
+        User result = editUserBasicInformationUseCase.execute(new EditUserBasicInformationCommand(
+                "Ferley",
+                "Joan",
+                "Mosquera Lozano",
+                "male",
+                "example@domain.com"
+        ));
+
+        assertEquals("Ferley", result.getFirstName());
+    }
+}
